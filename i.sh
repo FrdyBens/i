701c34b05d05f10615f2181bc4199294873a6a50
FrdyBens <140224457+FrdyBens@users.noreply.github.com>
2024-06-15 02:21:42 +0300

    Update i.sh

--- i.sh
+++ i.sh
@@ -1,5 +1,12 @@
+# install apache2
+
+
+
+
 sudo apt install apache2
+#install mysql
 sudo apt install mysql-server
+#remove any docker exsistance
 for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
 # Add Docker's official GPG key:
 sudo apt-get update
@@ -13,11 +20,16 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+#install docker
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-
+# git pull workspaces-images
 git clone https://github.com/kasmtech/workspaces-images.git /home/installs
-
+#install portainer to a specific port
 docker volume create portainer_data
 
 docker run -d -p 1024:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
+## not neccessary 
+# this twogoes together
+sudo apt-get install s3fs
+apt install libnet-amazon-s3-tools-perl
