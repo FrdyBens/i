e3a1577c2558b29dffc9db8a7896a2bfaebece8f
FrdyBens <140224457+FrdyBens@users.noreply.github.com>
2024-06-15 03:23:41 +0300

    Create docker.sh

---
+++ docker.sh
@@ -0,0 +1,24 @@
+for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+#install docker
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+# git pull workspaces-images
+git clone https://github.com/kasmtech/workspaces-images.git
+
+cd ~/i/workspaces-images
+cd workspaces-images
+
+
+sudo docker run --rm -d -it --shm-size=512m -p 6901:6901 -e VNC_PW=password kasmweb/firefox:dev
